bf547ead32ad44456146acb48f6de9a5a6e2da1b
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-03-08 13:54:14 +0530

    adding the git ignore file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const http = require('http');
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello World!'));
 
-// http.createServer((req,res)=>{
-//     res.write('<h1>hey this is vishnu</h1>');
+// const http = require('http');
+// // const chalk = require('chalk');
+
+
+// // http.createServer((req,res)=>{
+// //     res.write('<h1>hey this is vishnu</h1>');
+// //     res.end();
+// // }).listen(4000);
+
+// const server = ((req,res) =>{
+//     res.write('<h1>hey this is </h1>');
 //     res.end();
-// }).listen(4000);
+// })
 
-const server = ((req,res) =>{
-    res.write('<h1>hey this is </h1>');
-    res.end();
-})
-
-http.createServer(server).listen(4000);
+// http.createServer(server).listen(4000);
