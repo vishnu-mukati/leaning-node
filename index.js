313c04481b15e2067338eb33f15d970a764137dd
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-03-13 17:19:36 +0530

    create or remove input from the command line

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+
+const input = process.argv;
+
+if(input[2]=='add'){
+    fs.writeFileSync(input[3],input[4]);
+}else if (input[2] == 'remove'){
+    fs.unlinkSync(input[3]);
+}else{
+    console.log('invalid input');
+}
+
+
 // const chalk = require('chalk');
  
 // console.log(chalk.red('Hello World!'));
@@ -18,15 +31,15 @@
 
 // // http.createServer(server).listen(4000);
 
-const http = require('http');
-const data = require('./APIdata');
+// const http = require('http');
+// const data = require('./APIdata');
 
-const server = http.createServer((req,res)=>{
-    //Header of the response 
-    res.writeHead(200,{'Content-type' : 'application/json'});
-    // Body of the response
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(4000);
+// const server = http.createServer((req,res)=>{
+//     //Header of the response 
+//     res.writeHead(200,{'Content-type' : 'application/json'});
+//     // Body of the response
+//     res.write(JSON.stringify(data));
+//     res.end();
+// }).listen(4000);
 
 
